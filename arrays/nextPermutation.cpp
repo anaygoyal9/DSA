b40296a42f6cceb5a87092c12a239c06e7b97047
Anay Goyal <anaygoyal9@gmail.com>
2026-08-27 19:02:18 +0530

    bug

--- arrays/nextPermutation.cpp
+++ arrays/nextPermutation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 vector<int> nextPermutation(vector<int> &nums){
     int n = nums.size();
     int idx = -1;
-    for(int i= n-2  ;i>=0;i--){
+    for(int i= n-2 ;i>=0;i--){
         if(nums[i] < nums[i+1]){
             idx = i;
             break;
