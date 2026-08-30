66afcc38efae8acbcf8d24d68d2f16ab336a3281
Anay Goyal <anaygoyal9@gmail.com>
2026-08-30 23:51:06 +0530

    recursion but no dp

--- dp/multiplyNby2.cpp
+++ dp/multiplyNby2.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std;      // ------ Live example where recursion is there but no dp --------
 int MultiBy2(int n){
     if(n==1) return 2;
     return 2 + MultiBy2(n-1);
